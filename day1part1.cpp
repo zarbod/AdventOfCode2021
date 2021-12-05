113a47a5bbaf600ebdddfb3311fd6f546423aa4a
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2021-12-04 23:01:44 -0600

    Update day1part1.cpp

--- day1part1.cpp
+++ day1part1.cpp
@@ -1,3 +1,6 @@
+//
+// Created by aaroh on 12/2/2021.
+//
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -7,35 +10,25 @@ using namespace std;
 
 int main() {
     ifstream inputFile; inputFile.open("input1.txt");
-
-    vector<int> values;
     string lineString;
-    while(inputFile) {
-        inputFile >> lineString;
-        stringstream number(lineString);
-        int x = 0;
-        number >> x;
-        values.push_back(x);
+    int i = 0;
+    int lastNum = 0;
+    int sum = 0;
+    if ( inputFile.is_open() ) {
+        while(inputFile){
+            inputFile >> lineString;
+            stringstream number(lineString);
+            int x = 0;
+            number >> x;
+            if(i == 0){
+                i++;
+                lastNum = x;
+            }
+            else {
+                if (lastNum < x)
+                    sum++;
+                lastNum = x;
+            }
+        }
+        cout << sum;
     }
-
-    values.pop_back();
-
-
-    int i = 1;
-
-    int sum1, sum2;
-    int count = 0;
-    sum1 = values.at(0) + values.at(1) + values.at(2);
-
-    while(k < values.size()) {
-        sum2 = values.at(i) + values.at(i+1) + values.at(i+2);
-        if(sum2 > sum1)
-            count++;
-        i++;
-        sum1 = sum2;
-    }
-
-    cout << count;
-}
-
-
